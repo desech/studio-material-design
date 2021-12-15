7d70e6c28080bbda6a0f06e323c56b667e5a0c7b
catalin <catalin.luntraru@desech.com>
2021-12-15 17:42:41 +0200

    init other components

--- dist/design-system.js
+++ dist/design-system.js
@@ -6,4 +6,18 @@
 // https://material.io/develop/web/supporting/auto-init
 document.addEventListener('DOMContentLoaded', () => {
   window.mdc.autoInit()
+
+  // some components need to be manually instantiated
+  // https://material.io/develop/web/getting-started
+  const data = [
+    { class: 'mdc-tab-bar', mainObj: 'tabBar', subObj: 'MDCTabBar' },
+    { class: 'mdc-tab-scroller', mainObj: 'tabScroller', subObj: 'MDCTabScroller' },
+    { class: 'mdc-tab', mainObj: 'tab', subObj: 'MDCTab' },
+    { class: 'mdc-tab-indicator', mainObj: 'tabIndicator', subObj: 'MDCTabIndicator' }
+  ]
+  for (const record of data) {
+    for (const node of document.getElementsByClassName(record.class)) {
+      window.mdc[record.mainObj][record.subObj].attachTo(node)
+    }
+  }
 })
