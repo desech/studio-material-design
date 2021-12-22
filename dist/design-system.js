7efe6c6741b4cc147124bc5d0e4117911fe64442
catalin <catalin.luntraru@desech.com>
2021-12-22 14:56:11 +0200

    components update

--- dist/design-system.js
+++ dist/design-system.js
@@ -5,19 +5,30 @@
 // components need to have data-mdc-auto-init="MDCTextField", where MDCTextField is the class name
 // https://material.io/develop/web/supporting/auto-init
 document.addEventListener('DOMContentLoaded', () => {
-  window.mdc.autoInit()
+  window.mdc.autoInit(document, () => {})
 
   // some components need to be manually instantiated
   // https://material.io/develop/web/getting-started
-  const data = [
+  const attachData = [
     { class: 'mdc-tab-bar', mainObj: 'tabBar', subObj: 'MDCTabBar' },
     { class: 'mdc-tab-scroller', mainObj: 'tabScroller', subObj: 'MDCTabScroller' },
     { class: 'mdc-tab', mainObj: 'tab', subObj: 'MDCTab' },
     { class: 'mdc-tab-indicator', mainObj: 'tabIndicator', subObj: 'MDCTabIndicator' }
   ]
-  for (const record of data) {
+  for (const record of attachData) {
     for (const node of document.getElementsByClassName(record.class)) {
       window.mdc[record.mainObj][record.subObj].attachTo(node)
+    }
+  }
+
+  // some components need to be specifically opened
+  const openData = [
+    { class: 'mdc-banner', mainObj: 'banner', subObj: 'MDCBanner' }
+  ]
+  for (const record of openData) {
+    for (const node of document.getElementsByClassName(record.class)) {
+      const obj = new window.mdc[record.mainObj][record.subObj](node)
+      obj.open()
     }
   }
 })
