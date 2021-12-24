2afadcdabd83d784082079199e02f894f34607e9
catalin <catalin.luntraru@desech.com>
2021-12-24 14:37:33 +0200

    sync with desech

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-const Plugin = require('./lib/Plugin.js')
-
 // this is executed by electron's node.js, so we can't use npm packages,
 // only the ones available in Desech (check readme file)
 module.exports = {
-  enablePlugin (lib) {
-    this.uninstallPlugin(lib)
-    // sync files
-  },
-
-  updatePlugin (lib) {
-    this.installPlugin(lib)
-  },
-
-  uninstallPlugin (lib) {
-    // remove files
+  syncOpenProject (data, lib) {
+    lib.fse.copySync('./dist', data.folder)
   }
 }
