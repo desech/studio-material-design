e953e338501e3983fc189966b6dcd96c59962d51
catalin <catalin.luntraru@desech.com>
2021-12-24 15:29:45 +0200

    update paths

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 // this is executed by electron's node.js, so we can't use npm packages,
 // only the ones available in Desech (check readme file)
 module.exports = {
   syncOpenProject (data, lib) {
-    lib.fse.copySync('./dist', data.folder)
+    const src = path.resolve(__dirname, 'dist')
+    lib.fse.copySync(src, data.folder)
   }
 }
